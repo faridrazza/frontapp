98da969b539f940cb799d71d6467d87a5d21f86d
faridrazza <mdfarid.razza786@gmail.com>
2025-01-16 00:56:56 +0530

    remove prevous files 2024

--- lib/features/auth/presentation/widgets/social_login_button.dart
+++ lib/features/auth/presentation/widgets/social_login_button.dart
@@ -1,44 +1,44 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
-class SocialLoginButton extends StatelessWidget {
-  final String icon;
-  final String label;
-  final VoidCallback onPressed;
+// class SocialLoginButton extends StatelessWidget {
+//   final String icon;
+//   final String label;
+//   final VoidCallback onPressed;
 
-  const SocialLoginButton({
-    Key? key,
-    required this.icon,
-    required this.label,
-    required this.onPressed,
-  }) : super(key: key);
+//   const SocialLoginButton({
+//     Key? key,
+//     required this.icon,
+//     required this.label,
+//     required this.onPressed,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.white.withOpacity(0.1),
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        elevation: 0,
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(icon, height: 24, width: 24),
-          const SizedBox(width: 8),
-          Text(
-            label,
-            style: GoogleFonts.inter(
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-} 
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(
+//       onPressed: onPressed,
+//       style: ElevatedButton.styleFrom(
+//         backgroundColor: Colors.white.withOpacity(0.1),
+//         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(12),
+//         ),
+//         elevation: 0,
+//       ),
+//       child: Row(
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           Image.asset(icon, height: 24, width: 24),
+//           const SizedBox(width: 8),
+//           Text(
+//             label,
+//             style: GoogleFonts.inter(
+//               color: Colors.white,
+//               fontWeight: FontWeight.w500,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// } 
